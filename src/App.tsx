4c77c80784e676bcb3c3c786430a6f578ff4f62e
anonymous <anuwong@dollarsign.co>
2022-09-16 15:54:10 +0700

    refactor: separate Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Footer from "./components/Footer";
 
 function NavBar() {
   return (
@@ -10,38 +11,6 @@ function NavBar() {
         <li>Menu 3</li>
       </ul>
     </nav>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="bg-slate-600">
-      <div className="container mx-auto p-4">
-        <div className="flex flex-row justify-center items-center">
-          <div className="flex flex-col w-full justify-center items-center">
-            <ul>
-              <li>footer 1-1</li>
-              <li>footer 1-2</li>
-              <li>footer 1-3</li>
-            </ul>
-          </div>
-          <div className="flex flex-col w-full justify-center items-center ">
-            <ul>
-              <li>footer 2-1</li>
-              <li>footer 2-2</li>
-              <li>footer 2-3</li>
-            </ul>
-          </div>
-          <div className="flex flex-col w-full justify-center items-center ">
-            <ul>
-              <li>footer 3-1</li>
-              <li>footer 3-2</li>
-              <li>footer 3-3</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+function Footer() {
+  return (
+    <footer className="bg-slate-600">
+      <div className="container mx-auto p-4">
+        <div className="flex flex-row justify-center items-center">
+          <div className="flex flex-col w-full justify-center items-center">
+            <ul>
+              <li>footer 1-1</li>
+              <li>footer 1-2</li>
+              <li>footer 1-3</li>
+            </ul>
+          </div>
+          <div className="flex flex-col w-full justify-center items-center ">
+            <ul>
+              <li>footer 2-1</li>
+              <li>footer 2-2</li>
+              <li>footer 2-3</li>
+            </ul>
+          </div>
+          <div className="flex flex-col w-full justify-center items-center ">
+            <ul>
+              <li>footer 3-1</li>
+              <li>footer 3-2</li>
+              <li>footer 3-3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
